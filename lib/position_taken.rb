b3aee5a40a0e96e9bb9bb902bb38ac37a3248df8
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-20 23:04:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,11 @@
 # code your #position_taken? method here!
-
-board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def position_taken?(board_array, space_chosen_int)
-  if board_array space_chosen_int
-    puts "Please select another move"
-    
-else 
   # if not, do something else 
+
+def position_taken?(board,index)
+end
+
+
+  
+
+
+
