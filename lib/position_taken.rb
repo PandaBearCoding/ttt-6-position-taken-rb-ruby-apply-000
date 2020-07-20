89c760f35027b99e0608d00ca0497f4289dc59b6
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-20 22:51:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 # code your #position_taken? method here!
 
+board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def position_taken?
-  if 
+def position_taken?(board_array)
+  if
     # if this space is empty, allow move
 else 
   # if not, do something else 
